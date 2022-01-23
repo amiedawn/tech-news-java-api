c11e5a159c8419245b2ba9fd6fd1d113ad567493
amiedawn <amie.d.carroll@gmail.com>
2022-01-23 11:10:32 -0700

    Added models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,11 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 5.2;
+        double sum = 0;
+        sum = a + b;
+        System.out.println(sum);
+    }
+}
